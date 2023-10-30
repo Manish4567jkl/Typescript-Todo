9bbdb45497fa179351e649ad2dadf2874251a8d5
Manish4567jkl <manishmandal895@gmail.com>
2023-10-30 23:32:39 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
 import './style.css'
+
+const taskContainer = document.querySelector('.task-container') as HTMLDivElement;
+const input = document.getElementsByTagName('input')[0] as unknown  as HTMLInputElement; 
+const btn = document.querySelector('button') as HTMLButtonElement;
+const form = document.querySelector('form') as HTMLFormElement;
+
+interface Todo{
+    title:string,
+    isCompleted:boolean,
+    id:string
+}
+
+const todos:Todo[] =  [];
+
+form.onsubmit = (e:SubmitEvent) => {
+    e.preventDefault();
+
+    const todo:Todo = {
+        title:"",
+        isCompleted:false,
+        id:(Math.random()*100000).toString()
+    };
+
+    todos.push(todo);
+    input.value=""
+    console.log(todo)
+}
